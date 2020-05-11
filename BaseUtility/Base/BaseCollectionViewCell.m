46f6480a7aa6fb85894b69c02596c5ee793f6e6e
Singularity <>
2020-05-11 15:31:59 +0800

    baseCell

---
+++ BaseUtility/Base/BaseCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  BaseCollectionViewCell.m
+//  BaseTools
+//
+//  Created by mac on 2020/5/11.
+//  Copyright © 2020 Singularity. All rights reserved.
+//
+
+#import "BaseCollectionViewCell.h"
+
+@implementation BaseCollectionViewCell
+
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor=[UIColor whiteColor];
+        
+        [self setUI];
+    }
+    return self;
+}
+
+
+- (void)setUI{
+    
+}
+
+
+
+@end
